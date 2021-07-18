c0fda98e239aa6b675725d008a5744489cc7bfde
Eleonore <eleonore.caclard@gmail.com>
2021-07-18 17:17:46 -0400

    Final version for each file

---
+++ js/questions.js
@@ -0,0 +1,146 @@
+// array with# and questions&answer and the options
+let questions = [{
+        numb: 1,
+        question: "Which of the below is used in Javascript to insert special characters?",
+        answer: '/',
+        options: [
+            '&',
+            "/",
+            '-',
+            '%'
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "How do you find the number with the greatest value of  'a' and 'b'",
+        answer: "Math.max(a,b)",
+        options: [
+            "Math.ceil(a,b)",
+            "ceil(a,b)",
+            "top(a,b)",
+            "Math.max(a,b)"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "JavaScript ignores extra spaces",
+        answer: "True",
+        options: [
+            "True",
+            "False",
+            "This a easy one",
+            "I am not sure!"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What is the correct way to call the random method on the Math global object?",
+        answer: "Math.random()",
+        options: [
+            "random.Math()",
+            "Math.random()",
+            "math.random()",
+            "Math(random)"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Which of the following functions is a valid function type that javascript supports?",
+        answer: "1 and 2",
+        options: [
+            "A named function",
+            "An Anonymous function",
+            "1 and 2",
+            "None of the above"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "How can you add a comment in a JavaScript code?",
+        answer: "//My JavaScript comment",
+        options: [
+            "/*My JavaScript comment*/",
+            "//My JavaScript comment",
+            '{_comment1": "my JavaScript comment}',
+            'My JavaScript comment'
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "JavaScript is identical to Java?",
+        answer: "False",
+        options: [
+            "True",
+            "False",
+            "You Tell Me!",
+            "I am not sure!"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Which of the ways below is incorrect of instantiating a date?",
+        answer: "new Date(seconds)",
+        options: [
+            'new Date(dateString)',
+            'new Date()',
+            'new Date(seconds)',
+            'new Date(year, month, day, hours, minutes, seconds, milliseconds)'
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "How to round the number 15.2021 to a closer whole number?",
+        answer: "Math.round(15.2021)",
+        options: [
+            "Math.round(15.2021)",
+            "Math.rnd(15.2021)",
+            "Math.sin(15.2021)",
+            "Math.sqrt(15.2021)"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "What is the code to get a random number between 5 and 9 inclusive?",
+        answer: "Math.floor((Math.random()*5+5)",
+        options: [
+            "Math.floor((Math.random()*5+5)",
+            "Math.floor((Math.random()*5+4)",
+            "Math.floor((Math.random()*4+4)",
+            "Math.floor((Math.random()*4+5)"
+        ]
+    },
+
+    {
+        numb: 11,
+        question: "How many parameters can be passed to a function?",
+        answer: "1 for each argument",
+        options: [
+            "None",
+            "As much as you want",
+            "1 for each argument",
+            "A maximum of 3"
+        ]
+    },
+
+    {
+        numb: 12,
+        question: "Which of the following is not a mouse event?",
+        answer: "onmousescroller",
+        options: [
+            "onclick",
+            "onmouse",
+            "onbutton",
+            "onmousescroller"
+        ]
+    },
+
+];
